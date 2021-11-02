df7f3b3b31a73645ee71818f4408eecb4340599b
soralee <sora2821@gmail.com>
2021-11-02 14:59:09 +0900

    subdivide function & print function results

--- mission1.js
+++ mission1.js
@@ -1,28 +1,51 @@
-function getArea(figure, ...arguments) {
+let results = [];
+
+getArea('circle', results, 10);
+getArea('rect', results, 10, 15);
+getArea('trapezoid', results, 10, 15, 12);
+getArea('circle', results, 1, 2);
+
+printExecutionSequence();
+
+function getArea(figure, results, ...arguments) {
+    const parameters = [...arguments];
+
     if (figure === 'circle') {
-        if (arguments.length === 1) {
-            console.log(Math.PI * (arguments[0] ** 2));
-        } else {
-            let answer = 0;
-            for (let radius = arguments[0]; radius <= arguments[1]; radius++) {
-                answer += Math.PI * (radius ** 2);
-            }
-            console.log(answer);
-        }
+        results.push('circle');
+        results.push(getCircleArea(parameters));
+        return results;
     } else if (figure === 'rect') {
-        console.log(arguments[0] * arguments[1]);
+        results.push('rect');
+        results.push(getRectArea(parameters));
+        return results;
     } else if (figure === 'trapezoid') {
-        console.log((arguments[0] + arguments[1]) * arguments[2] / 2);
+        results.push('trapezoid');
+        results.push(getTrapezoidArea(parameters));
+        return results;
     }
 }
 
-function printExecutionSequence() {
-    
+function getCircleArea(parameters) {
+    let result = 0;
+    if (parameters.length === 1) {
+        result = Math.PI * Math.pow(parameters[0], 2);
+        return result;
+    } else if (parameters.length === 2) {
+        for (let radius = parameters[0]; radius <= parameters[1]; radius++) {
+            result += Math.PI * Math.pow(radius, 2);
+        }
+        return result;
+    }
 }
 
-getArea('circle', 10);
-getArea('rect', 10, 15);
-getArea('trapezoid', 10, 15, 12);
-getArea('circle', 1, 2);
+function getRectArea(parameters) {
+    return parameters[0] * parameters[1];
+}
 
-printExecutionSequence();
+function getTrapezoidArea(parameters) {
+    return ((parameters[0] + parameters[1]) * parameters[2] / 2);
+}
+
+function printExecutionSequence() {
+    console.log(results);
+}
