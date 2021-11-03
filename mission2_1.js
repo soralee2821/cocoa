78d7371d0ed2fde45304723b9450601769529b81
soralee <sora2821@gmail.com>
2021-11-03 17:41:15 +0900

    mission 2_1 gildong number

--- mission2_1.js
+++ mission2_1.js
@@ -1,15 +1,20 @@
 function solution(n, t, m, p) {
-    let numberArray = [];
+  let numberArray = [];
+  let gildongNumber = [];
 
-    for (let number = 1; number <= t; number++) {
-        let convertedNumber = number.toString(n);
-        convertedNumber = convertedNumber.split('');
-        numberArray.push(...convertedNumber);
-        numberArray.map((item) => {
-            console.log(item, index);
-        });
-    }
-    return numberArray;
+  for (let number = 1; number <= t; number++) {
+      let convertedNumber = number.toString(n);
+      convertedNumber = convertedNumber.split('');
+      numberArray.push(...convertedNumber);
+  }
+  numberArray.forEach((element, index) => {
+    if ((index + 1) % m === p) gildongNumber.push(element);
+  })
+
+  return {
+    totalNumber : numberArray,
+    gildong : gildongNumber,
+  };
 }
 
-console.log(solution(2,3));
+console.log(solution(2, 5, 3, 2));
