ab860d003e37b29cef19faff9b7615886386dfac
soralee <sora2821@gmail.com>
2021-11-04 14:36:29 +0900

    mission1 divide function

--- mission2_1.js
+++ mission2_1.js
@@ -1,20 +1,29 @@
 function solution(n, t, m, p) {
+  const numberArray = getNumericalSystem(n, t);
+  return {
+    totalNumber : numberArray,
+    gildong : findGildong(m, p, numberArray)
+  };
+}
+
+function getNumericalSystem(n, t) {
   let numberArray = [];
-  let gildongNumber = [];
 
   for (let number = 1; number <= t; number++) {
-      let convertedNumber = number.toString(n);
-      convertedNumber = convertedNumber.split('');
-      numberArray.push(...convertedNumber);
+    let convertedNumber = number.toString(n);
+    convertedNumber = convertedNumber.split('');
+    numberArray.push(...convertedNumber);
   }
+  return numberArray;
+}
+
+function findGildong(m, p, numberArray) {
+  let gildongNumber = [];
+
   numberArray.forEach((element, index) => {
     if ((index + 1) % m === p) gildongNumber.push(element);
   })
-
-  return {
-    totalNumber : numberArray,
-    gildong : gildongNumber,
-  };
+  return gildongNumber;
 }
 
 console.log(solution(2, 5, 3, 2));
