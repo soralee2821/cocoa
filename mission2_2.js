c852028d4e29d14228169e6c85457d45edc47e17
soralee <sora2821@gmail.com>
2021-11-04 21:02:06 +0900

    delete comments

--- mission2_2.js
+++ mission2_2.js
@@ -67,10 +67,10 @@ function deleteNumber(peopleID) {
   return filteredID;
 }
 
+
 // 3. calculate average
 const grades = [[88,76,77], [33,44,44], [90,100,94], [30,44,98]];
 
-// calaculate the average of each student
 let averageArray = [];
 grades.forEach(function (student) {
   let studentAverage = student.reduce((a, b) => a + b, 0) / student.length;
@@ -79,7 +79,6 @@ grades.forEach(function (student) {
 });
 console.log("모든 학생의 평균 점수 목록 : ",averageArray);
 
-// calculate the average of maximum values of each student
 let maximumArray = grades.map((student) => Math.max(...student));
 const totalMaximumValue = maximumArray.reduce((a, b) => a + b, 0) / maximumArray.length;
 console.log("모든 학생의 최고 점수의 평균값 : ",totalMaximumValue);
