ab871c1a2a429d7ee2e82f9347c993e70e623f0f
soralee <sora2821@gmail.com>
2021-11-06 14:27:21 +0900

    make reduce function

---
+++ week1/Array&Object/mission2_6.js
@@ -0,0 +1,14 @@
+const myReduce = (arr, callback, initialValue) => {
+    if (!Array.isArray(arr)) {
+        throw TypeError (`${arr} is not an array.`);
+    }
+    let accumulator = initialValue === undefined ? arr[0] : initialValue;
+    let index = initialValue === undefined ? 1 : 0;
+    for (index; index < arr.length; index++) {
+        accumulator = callback(accumulator, arr[index]);
+    }
+    return accumulator;
+}
+
+const result = myReduce([1, 2, 3], (a, b) => a + b, 0);
+console.log(result);
