6f6e507c4967aba4279998f0197991ff0301b13d
soralee <sora2821@gmail.com>
2021-11-17 00:32:32 +0900

    add whether is going to continue

--- week3/async/usergrade.js
+++ week3/async/usergrade.js
@@ -11,25 +11,24 @@ const grade = {
   math : [],
   english : [],
 }
-let koreanArray = [];
-let mathArray = [];
-let englishArray = [];
 
-const question1 = () => {
+const setSubject = () => {
   return new Promise((resolve, reject) => {
-    rl.question('What is your subject? ', (answer) => {
-      if (answer === 'korean' || answer === 'Korean') {
+    rl.question('What is your subject? (Korean/ Math/ English)  ', (answer) => {
+      if (answer === 'Korean' || answer === 'korean') {
         resolve(grade.korean);
-      } else if (answer === 'math' || answer === 'Math') {
+      } else if (answer === 'Math' || answer === 'math') {
         resolve(grade.math);
-      } else if (answer === 'english' || answer === 'English') {
+      } else if (answer === 'English' || answer === 'english') {
         resolve(grade.english);
+      } else {
+        throw new Error ('you write the wrong subject!');
       }
     });
   });
 }
 
-const question2 = (array) => {
+const setGrade = (array) => {
   return new Promise((resolve, reject) => {
     rl.question('What is your grade? ', (answer) => {
       array.push(+answer);
@@ -38,14 +37,46 @@ const question2 = (array) => {
   })
 }
 
-const main = async () => {
-  let gradeArray = await question1().then(question2);
+const askQuestion = async () => {
+  let gradeArray = await setSubject().then(setGrade);
   console.log(gradeArray);
 }
 
 const excute = async () => {
-  await main().then(main);
-  rl.close();
+  await askQuestion().then(askQuestion).then(askQuestion).then(askExcution);
+}
+
+const askExcution = () => {
+  rl.question('Are you going to continue? (Yes / No)  ', (answer) => {
+    if (answer === 'Yes' || answer === 'yes') {
+      excute();
+    } else if (answer === 'No' || answer === 'no') {
+      rl.close();
+      checkGrade(grade);
+    } else {
+      throw new Error ('It is the wrong answer!');
+    }
+  });
+}
+const checkGrade = (grade) => {
+  if (grade.korean.length >= 2) {
+    calculateGrade('Korean', grade.korean);
+  }
+  if (grade.math.length >= 2) {
+    calculateGrade('Math', grade.math);
+  }
+  if (grade.english.length >= 2) {
+    calculateGrade('English', grade.english);
+  }
+}
+
+const calculateGrade = (subject, gradeArray) => {
+  const gradeProgram = new GradeProgram(gradeArray);
+  console.log(`${subject} average : `, gradeProgram.getMean().toFixed(2));
+  console.log(`${subject} standard deviation : `, gradeProgram.getStandardDeviation().toFixed(2));
+  console.log(`${subject} Z-score 70 : `, gradeProgram.getZscore(70).toFixed(2));
+  console.log(`${subject} Z-score 80 : `, gradeProgram.getZscore(80).toFixed(2));
+  console.log(`${subject} probability from 70 to 80 : `, gradeProgram.getPercentage(70, 80));
 }
 
 excute();
