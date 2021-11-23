cef95243d89e1f61a2b39d40462a784da79b056d
soralee <sora2821@gmail.com>
2021-11-23 10:16:03 +0900

    first try

---
+++ week4/MVCclass/index.js
@@ -0,0 +1,107 @@
+/*
+[] Model, View, Controller Class로 나누기
+[] 수정 기능 추가
+[] 할일 목록, 한일 목록으로 나누기
+[] 날짜별 ToDo 보는 기능 만들기
+[] CSS - 레이어 카드 UI로 만들기
+*/
+
+class TodoModel {
+  constructor() {
+    this.todos = JSON.parse(localStorage.getItem('21.11.22')) || [];
+  }
+
+  addTodo(todoText) {
+    const todo = {
+      id: this.todos.length > 0 ? this.todos[this.todos.length - 1].id + 1 : 1,
+      text: todoText,
+      complete: false,
+    }
+    this.todos.push(todo);
+  }
+
+  editTodo(id, updatedText) {
+    this.todos = this.todos.map((todo) => {
+      todo.id === id ? {id: todo.id, text, updatedText, complete: false} : todo;
+    });
+  }
+
+  deleteTodo(id) {
+    this.todos = this.todos.filter((todo) => todo.id !== id);
+  }
+
+}
+
+class TodoView {
+  createElement(tag, className) {
+    const $element = document.createElement(tag);
+    if (className) $element.classList.add(className);
+    return $element;
+  }
+
+  displayTodo(todo) {
+    const $todoList = getElementById("todo-list");
+    const taskList = this.createElement("li", "task-list");
+
+    const checkbox = this.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.checked = todo.complete;
+
+    const span = this.createElement("span");
+    span.value = todo.text;
+
+    const garbageButton = this.createElement("button", "garbage-button");
+    const garbageImage = this.createElement("img");
+    garbageImage.src = "../image/garbage.jpg";
+    garbageImage.alt = "garbage-image";
+    garbageButton.append(garbageImage);
+
+    taskList.append(checkbox, span, garbageButton);
+    $todoList.append(taskList);
+  }
+}
+
+class TodoController {
+  constructor(model, view) {
+    this.model = model;
+    this.view = view;
+  }
+
+  displayTodoList() {
+    this.model.todos.forEach((todo) => {
+      this.view.displayTodo(todo);
+      this.handleCheckTodo(todo);
+      this.handleDeleteTodo(todo);
+    });
+  }
+
+  handleAddTodo() {
+    const $addButton = document.getElementById("add-button");
+    $addButton.addEventListener("click", () => {
+      const $newTaskInput = document.getElementById("new-task");
+      this.model.addTodo($newTaskInput.value);
+    })
+  }
+
+  handleCheckTodo(todo) {
+    const checkbox = todo.children[0];
+    checkbox.addEventListener("change", () => {
+      if (checkbox.checked) {
+        checkbox.parentElement.classList.add("line-through");
+      } else {
+        checkbox.parentElement.classList.remove("line-through");
+      }
+    });
+  }
+
+  handleDeleteTodo(todo) {
+    const id = todo.id;
+    this.model.deleteTodo(id);
+  }
+}
+
+const model = new TodoModel();
+const view = new TodoView();
+const controller = new TodoController(model, view);
+controller.displayTodoList();
+controller.handleAddTodo();
